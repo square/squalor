ed9e69cd2d5de6c810ea77f69dee239b5a790cd2
Martin Ahrens <mpa@squareup.com>
2024-10-17 13:21:07 -0400

    change TestTransactionBlockFailsOnDuplicatePrimaryKeyUpdate back to equals

--- db_test.go
+++ db_test.go
@@ -1079,16 +1079,15 @@ func TestTransactionBlockFailsOnDuplicatePrimaryKeyUpdate(t *testing.T) {
 		t.Fatal(err.Error())
 	}
 
-	expectedErrorPrefix := fmt.Sprintf("Error 1062 (23000): Duplicate entry '%v' for key", u2.ID)
+	expectedError := fmt.Sprintf("Error 1062 (23000): Duplicate entry '%v' for key 'users.PRIMARY'", u2.ID)
 
 	if err := db.Transaction(func(tx *Tx) error {
 		_, err := db.Exec(fmt.Sprintf("UPDATE `users` SET id = %v WHERE `id` = %v", u2.ID, u1.ID))
 
 		return err
-	}); err == nil || strings.Index(err.Error(), expectedErrorPrefix) == -1 {
-		t.Fatalf("Expected %v error prefix, got: %v", expectedErrorPrefix, err)
+	}); err == nil || err.Error() != expectedError {
+		t.Fatalf("Expected %v error, got: %v", expectedError, err)
 	}
-
 }
 
 func TestTransactionBlockRecoversFromPanic(t *testing.T) {
